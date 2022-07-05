055ccd110c7362abcdb8d4e81e0a395c238a4da5
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-07-05 16:44:03 -0700

    Update main.js
    Added RGB color generator. Also updated HEX function.

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const setBg = () => {
+// HEX
+
+function hexBg() {
   const randomColor = Math.floor(Math.random() * 6728667).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
-  color.innerHTML = "#" + randomColor;
+  color.innerHTML = "HEX:" + " #" + randomColor;
   console.log(randomColor);
-};
+}
 
-genNewBtn.addEventListener("click", setBg);
-setBg();
+genNewBtn.addEventListener("click", hexBg);
+hexBg();
+
+// RGB
+
+/*
+function rgbBg() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  document.body.style.backgroundColor =
+    "rgb" + "(" + r + "," + g + "," + b + ")";
+  color.innerHTML = "RGB: " + r + ", " + g + ", " + b;
+  console.log(r, ",", g, ",", b);
+}
+
+/*
+genNewBtn.addEventListener("click", rgbBg);
+rgbBg();
+*/
